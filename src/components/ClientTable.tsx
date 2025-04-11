d135a87c23a1122e971cc0475eaec029c74efc8d
Vinicius <83793571+viniciusxv27@users.noreply.github.com>
2025-04-11 19:28:36 -0300

    Alteração: link para botão

--- src/components/ClientTable.tsx
+++ src/components/ClientTable.tsx
@@ -63,7 +63,7 @@ export default function ClientTable({ clients, onSearch, filterBy }: ClientTable
                   <td className="px-6 text-left py-4 text-sm">
                     <a
                       href={`/clients/${client.id}`}
-                      className="text-blue-600 hover:underline text-sm"
+                      className="buttonDetail px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
                     >
                       Ver detalhes
                     </a>
